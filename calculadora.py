1216aa47b6163b8a47e8b42e3b7b73973a1020a5
Joaquim Miguel <74877588+Joaquim232@users.noreply.github.com>
2021-03-29 12:17:35 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,9 @@
+print('Olá! Seja bem vindo a Calculadora do Python!')
+print('Pressione ENTER para iniciar!')
+input()
+print('Ok, vamos lá!')
+n1 = int = input('Digite um valor: ')
+n2 = int = input('Digite outro valor: ')
+s = n1 + n2
+print('A Soma entre {} e {} é igual a {}!'.format(n1, n2, s))
+
---
+++ calculator.py
@@ -0,0 +1,9 @@
+print('Hello! Welcome to Python Calculator!')
+print('Press ENTER to begin!')
+input()
+print('Ok, lets go!')
+n1 = int = input('Type a value: ')
+n2 = int = input('Type another value: ')
+s = n1 + n2
+print('The sum between {} and {} equals {}!'.format(n1, n2, s))
+
